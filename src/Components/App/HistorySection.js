756a6b594d1e5383b5741bb203bd6f57d1f1c88e
Karen Garcia <saykaren@gmail.com>
2022-02-01 18:22:18 -0700

    Adds Holloway section

--- src/Components/App/HistorySection.js
+++ src/Components/App/HistorySection.js
@@ -6,7 +6,7 @@ const HistorySection = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [familyData, setFamilyData] = useState(FamilyHistory);
   // const [familyGenerationLevel, setFamilyGenerationLevel] = useState(400);
-  const levelArray = [400, 500, 600, 700, 800, 900, 1000];
+  const levelArray = [400, 500, 600, 700, 800, 900, 1000, 1100, 1200];
 
   return (
     <section>
