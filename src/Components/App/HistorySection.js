669b8bced2c0a3b4e154462a8219330c96c35f72
Karen Garcia <saykaren@gmail.com>
2022-02-05 22:12:34 -0700

    Update modal

--- src/Components/App/HistorySection.js
+++ src/Components/App/HistorySection.js
@@ -8,7 +8,7 @@ const HistorySection = () => {
   const [familyData, setFamilyData] = useState(FamilyHistory);
   const [modal, setModal] = useState(false);
   const [modalData, setModalData] = useState(1061);
-  const levelArray = [400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700];
+  const levelArray = [400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000];
   const updateModal = (newId)=>{
     setModalData(newId);
     setModal(true);
